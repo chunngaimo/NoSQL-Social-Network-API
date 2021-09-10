8510ab6449222e893cddda1fab4076e909d0c655
omen <wjy16888888@gmail.com>
2021-09-10 01:57:46 -0400

    MVP

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
 # Social Network API
+
+## Description
+
+---
+
+An API for a social network that uses a NoSQL database, which is able to handle large amounts of unstructured data.
+
+---
+
+## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Walkthrough Video](#walkthrough-video)
+  - [Screenshots/GIF](#Screenshots/GIF)
+  - [License](#License)
+  - [Contributing](#Contributing)
+  - [Questions](#Questions)
+
+---
+
+## Installation
+
+
+1. Clone the repository
+   
+2. Install mySQL
+   
+3. npm install
+
+---
+
+## Usage
+
+1. Setup the MongoDB database
+2. npm start
+
+---
+
+## Walkthrough Video
+
+Walk through video here - [Youtube Link](https://www.youtube.com/watch?v=DYJ3OeqlAW4)
+
+---
+
+## Screenshots
+
+![](source/images/noSQL.jpg)
+
+---
+
+## License
+
+This project is licensed under the [MIT License](https://choosealicense.com/licenses/mit).
+
+---
+
+## Contributing
+
+Contributions, issues and feature requests are welcome.
+
+---
+
+## Questions
+
+Here is my [GitHub](https://github.com/chunngaimo)
+and Feel free to ask any questions and contact me at wjy16888888@gmail.com.
---
+++ models/Thought.js
@@ -0,0 +1,62 @@
+const { Schema, model, Types } = require('mongoose');
+
+const dateFormat = require('dateformat');
+
+//schema for reactions
+const ReactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        max: [280, 'You have exceeded the 280 character limit!']
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdDate => dateFormat(createdDate, "dddd, mmmm dS, yyyy, h:MM:ss TT")
+    }
+});
+
+
+//schema for thoughts
+const ThoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        min: [1, 'Enter a valid thought please!'],
+        max: [280, 'You have exceeded the 280 character limit!']
+    },
+    createdAtDate: {
+        type: Date,
+        default: Date.now,
+        get: createdDate => dateFormat(createdDate, "dddd, mmmm dS, yyyy, h:MM:ss TT")
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    reactions: [ReactionSchema]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+//get reactionCount
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+
+//create the thought model
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
